5ae4d14d1270ded12b2800bd53776716ab4b22d9
rokosbasilisk <ram.bharadwaj.arya@gmail.com>
2023-05-20 13:57:31 +0530

    update .

---
+++ chargpt.py
@@ -0,0 +1,48 @@
+'''
+character level gpt 
+1) code with minimum description length
+2) think fast-and-slow
+
+'''
+import torch.nn as nn
+import torch 
+import numpy as np
+
+
+def encode(text):
+    pass
+
+def decode(text):
+    pass
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self):
+        pass 
+
+
+class MLP(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Block(nn.Module):
+    def __init__(self):
+        pass
+
+class Model(nn.Module):
+    def __init__(self):
+        pass
+
+
+'''
+show a single forward pass on a random text, encode and decode it
+'''
+
+class Trainer:
+    def __init__(self):
+        pass
+
+'''
+train the model using Trainer class 
+and run predictions on it
+'''
